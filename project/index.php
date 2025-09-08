2e2c9b0a33626c85825f6d4e60b25c0b16a37db0
nitin tiwari <nitintiwari0791@gmail.com>
2025-09-08 12:31:29 +0530

    Update index.php

--- project/index.php
+++ project/index.php
@@ -1,1 +1,21 @@
-
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome</title>
+  <link rel="stylesheet" href="./assest/css/style.css">
+</head>
+<body>
+<div class="welcome-box">
+  <h1>Welcome, <?php echo $_SESSION['username']; ?> 🎉</h1>
+  <a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
